5885cb6558feff904fc21e9a5cc2901cdb162bae
Christian Cain <christian22cain@yahoo.com>
2021-02-16 14:13:51 -0600

    added useCreateIndex option to cancel error

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const db = config.get('MongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(db, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
         console.log('MongoDB connected')
     } catch(err) {
         console.error(err.message);
