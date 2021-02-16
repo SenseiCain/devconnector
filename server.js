9426dee4cc14c99f78bfea0447ce6bdf2a87a17c
Christian Cain <christian22cain@yahoo.com>
2021-02-16 12:59:04 -0600

    set up db & routes

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }))
+
+// Define Routes
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 
 const PORT = process.env.PORT || 3000;
 
